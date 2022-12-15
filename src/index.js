e6d43cb96635effa1bba5a303a775f2144958a29
leomalave <leomalave1993@gmail.com>
2022-12-15 19:21:29 -0400

    fetch API

--- src/index.js
+++ src/index.js
@@ -8,10 +8,23 @@ import { Saludar } from "./Saludar";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+
+const handleChange = (e) => {
+  console.log(e.target.value)
+}
+
 root.render(
   <>
     <TaskCard ready={true} />
     <Saludar />
     <Button text="Saludar"/>
+
+    <form onSubmit={(e) => {
+      e.preventDefault()
+      alert('enviado')
+    }}>
+      <h1>Registro de usuario</h1>
+      <button>Send</button>
+    </form>
   </>
 );
