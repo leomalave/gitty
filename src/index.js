f5289e03c7dbc85a1b4e2ba49e4c1aae18ab16c1
leomalave <leomalave1993@gmail.com>
2022-12-17 12:16:36 -0400

    useState

--- src/index.js
+++ src/index.js
@@ -6,29 +6,17 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter(){
 
-  const [ counter, setCounter] = useState(10)
+  const [ mensaje, setMensaje] = useState('')
 
   
   return (
     <div>
-      <h1>Counter: { counter }</h1>
-    <button onClick={() => {
-      setCounter( counter + 1)
-    }}>
-      Sumar
-    </button>
-
-    <button onClick={() => {
-      setCounter( counter - 1)
-    }}>
-      Restar
-    </button>
-
-    <button onClick={() => {
-      setCounter(500)
-    }}>
-      Reiniciar
-    </button>
+      <input onChange={(e) => setMensaje(e.target.value)} />
+      <button onClick={() => {
+        alert('El mensaje es: ' + mensaje)
+      }}>
+        Save
+      </button>
     </div>
   )
 }
